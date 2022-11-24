dec07c90c0b0443f249cee2fcba582d0f9523cd4
mohammadhani18 <mhani18403@gmail.com>
2022-11-24 10:14:21 +0500

    Paddle and ball made

--- project.asm
+++ project.asm
@@ -15,14 +15,24 @@ Paddle STRUCT
     paddle_vel dw ?
 Paddle ENDS
 
+Brick STRUCT
+    brick_x dw ?
+    brick_y dw ?
+    brick_h dw ?
+    brick_w dw ?
+    brick_size dw ?
+Brick ENDS
+
+
 .model small
 .stack 100h
 .data
     ball_1 BALL <3,150,185,2,2>
     paddle_1 Paddle <3,50,130,190,6>      
+    brick_1 Brick <10,10,10,25,3>
 .code
 
-    MakeScreen MACRO 
+    MakeScreen MACRO ;for refreshong screen to blank
         mov ah, 00h
         mov al,13h
         int 10h
@@ -58,6 +68,31 @@ Paddle ENDS
             jng draw 
     ENDM
 
+    MakeBrick MACRO brick_obj
+        mov cx,brick_obj.brick_x
+        mov dx,brick_obj.brick_y
+
+        l2:
+            ;draw pixel
+            mov ah,0ch
+            mov al,0ch
+            int 10h 
+
+            ;loop
+            inc cx 
+            mov ax, cx
+            sub ax,brick_obj.brick_x
+            cmp ax,brick_obj.brick_w 
+            jng l2
+
+            inc dx 
+            mov cx, brick_obj.brick_x
+            mov ax, dx
+            sub ax,brick_obj.brick_y
+            cmp ax,brick_obj.brick_h 
+            jng l2
+    ENDM
+
     DrawPaddle MACRO paddle_obj
         mov cx, paddle_obj.paddle_x
         mov dx, paddle_obj.paddle_y 
@@ -84,6 +119,7 @@ Paddle ENDS
     ENDM
 
     MoveBall MACRO ball_obj
+        
         ;screen resolution = 320x200, (140h x 08ch)
         ;move ball in x direction
         mov ax, ball_obj.ball_vel_x
@@ -93,20 +129,37 @@ Paddle ENDS
         mov ax,ball_obj.ball_vel_y
         sub ball_obj.ball_y, ax
         
-        cmp ball_obj.ball_x, 10
-        jl negative
+        collisions:
+            cmp ball_obj.ball_x, 10h
+            je negative
+            cmp ball_obj.ball_x, 140h
+            je positive
 
-        cmp ball_obj.ball_y, 10
-        jl negative2
+            cmp ball_obj.ball_y, 10h
+            je negative2
 
-        ;cmp ball_obj.ball_y, 08ch 
+            cmp ball_obj.ball_y, 08ch
+            je positive2
+
         
         
-        negative:
-            neg ball_obj.ball_vel_x
-        negative2:    
-            neg ball_obj.ball_vel_y 
-             
+            negative:
+                mov ax, ball_obj.ball_vel_x
+                add ball_obj.ball_x, ax
+               
+            negative2:    
+                mov ax, ball_obj.ball_vel_y
+                add ball_obj.ball_y, ax
+            
+            positive:
+                mov ax, ball_obj.ball_vel_x
+                sub ball_obj.ball_x, ax
+            
+            positive2:   
+                mov ax, ball_obj.ball_vel_y
+                add ball_obj.ball_y, ax
+
+        l1:   
     ENDM
 
     MovePaddle MACRO paddle_obj
@@ -116,28 +169,35 @@ Paddle ENDS
         
         mov ah,00h
         int 16h
+        
+        cmp ah, 4Bh
+        je left 
+        
         cmp ah, 4Dh
         je right
 
-        cmp ah, 4Bh
-        je left 
-        jmp exit 
+        
+        jmp exit
        
         left:
             mov ax,paddle_obj.paddle_vel
             sub paddle_obj.paddle_x,ax 
+            jmp exit
         right:
             mov ax,paddle_obj.paddle_vel
             add paddle_obj.paddle_x,ax 
         exit:    
     ENDM
+
+    
+
 start:
 
     main proc 
         mov ax,@data
         mov ds,ax
-    
-        check_time:
+        
+        check_time: ;infinite loop to keep the game going
     
             mov ah,2ch ;get time of laptop
             int 21h
@@ -150,8 +210,29 @@ start:
             DrawPaddle paddle_1
             ;MoveBall ball_1
             DrawBall ball_1  
-        jmp check_time
+            MakeBrick brick_1
     
+            mov ah,01h
+            int 16h
+            jz check_time 
+            
+            mov ah,00h
+            int 16h
+            
+            cmp al, 20h ;if space is pressed lauch the ball 
+            je Move
+            
+            cmp al, 65h ;if e is pressed exit the game
+            je stopGame
+            
+            Move:
+                MoveBall ball_1
+            
+            jmp check_time
+    
+    stopGame:
+        mov ah,4ch
+        int 21h
     main endp
 
 end start    
